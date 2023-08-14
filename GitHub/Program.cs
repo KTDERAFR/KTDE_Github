30635f5848c8374e202db5070662824f36d3705c
jporock <131279409+jporock@users.noreply.github.com>
2023-08-14 14:26:09 +0200

    age

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!, This is my name");
+Console.WriteLine("Hello, World!, This is my name, this is my age");
